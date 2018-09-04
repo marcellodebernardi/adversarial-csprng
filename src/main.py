e7443dc4a4a29b0147362cdabe81355bba015980
marcellodebernardi <marcello1234@live.co.uk>
2018-09-04 15:02:18 +0300

    Implement output folder structure

--- src/main.py
+++ src/main.py
@@ -63,6 +63,7 @@ BETA2 = 0.9999
 # training settings
 STEPS = [10, 100, 1000, 10000, 100000]
 EXPERIMENT_REPETITIONS = 10
+EVAL_MILESTONES = 5
 ADV_MULT = 3
 EVAL_SEED = 10
 
@@ -74,79 +75,17 @@ GRAPH_DIR = '../output/tensor_board/'
 
 
 def main():
-    """ Constructs the neural networks, trains them, and logs all relevant information."""
+    """ Constructs the neural networks, trains them, and logs all relevant information. """
 
-    # num_of_batches = NUMBER_OF_BITS_PRODUCED / (batch_size * )
-    # EVAL_DATA = inputs.get_eval_input_numpy(10, EVAL_BATCHES, BATCH_SIZE)
-
-    # train discriminative GAN
-    try:
-        if sys.argv[1] == 'gan':
-            print('# GAN - batch size: ', BATCH_SIZE)
-            run_experiment(run_discgan)
-        # train predictive GAN
-        elif sys.argv[1] == 'predgan':
-            print('# PredGAN - batch size: ', BATCH_SIZE)
-            run_experiment(run_predgan)
-    except KeyboardInterrupt:
-        print('[INTERRUPTED BY USER]')
+    if sys.argv[1] == 'gan':
+        print('# GAN - batch size: ', BATCH_SIZE)
+        run_experiment(run_discgan)
+    elif sys.argv[1] == 'predgan':
+        print('# PredGAN - batch size: ', BATCH_SIZE)
+        run_experiment(run_predgan)
 
 
-def run_experiment(train_and_eval):
-    """ Calls the training and evaluation procedure repeatedly, iterating
-    over the possible modified values. """
-    steps = STEPS[0]
-    # defaults
-    num_of_elements = NUMBER_OF_BITS_PRODUCED / (OUTPUT_SIZE['default'] * OUTPUT_BITS['default'])
-    eval_data = inputs.get_eval_input_numpy(EVAL_SEED, num_of_elements, INPUT_SIZE['default'])
-    train_and_eval(INPUT_SIZE['default'], GEN_WIDTH['default'], OUTPUT_SIZE['default'], MAX_VAL['default'],
-                   BATCH_SIZE['default'], GEN_OPT(LEARNING_RATE['default'], BETA1, BETA2),
-                   OPP_OPT(LEARNING_RATE['default'], BETA1, BETA2), steps, eval_data)
-    # vary input size
-    for input_size in INPUT_SIZE['options']:
-        num_of_elements = NUMBER_OF_BITS_PRODUCED / (OUTPUT_SIZE['default'] * OUTPUT_BITS['default'])
-        eval_data = inputs.get_eval_input_numpy(EVAL_SEED, num_of_elements, input_size)
-        train_and_eval(input_size, GEN_WIDTH['default'], OUTPUT_SIZE['default'], MAX_VAL['default'],
-                       BATCH_SIZE['default'], GEN_OPT(LEARNING_RATE['default'], BETA1, BETA2),
-                       OPP_OPT(LEARNING_RATE['default'], BETA1, BETA2), steps, eval_data)
-    # vary gen width
-    num_of_elements = NUMBER_OF_BITS_PRODUCED / (OUTPUT_SIZE['default'] * OUTPUT_BITS['default'])
-    eval_data = inputs.get_eval_input_numpy(EVAL_SEED, num_of_elements, INPUT_SIZE['default'])
-    for gen_width in GEN_WIDTH['options']:
-        train_and_eval(INPUT_SIZE['default'], gen_width, OUTPUT_SIZE['default'], MAX_VAL['default'],
-                       BATCH_SIZE['default'], GEN_OPT(LEARNING_RATE['default'], BETA1, BETA2),
-                       OPP_OPT(LEARNING_RATE['default'], BETA1, BETA2), steps, eval_data)
-    # vary gen output size
-    for output_size in OUTPUT_SIZE['options']:
-        num_of_elements = NUMBER_OF_BITS_PRODUCED / (output_size * OUTPUT_BITS['default'])
-        eval_data = inputs.get_eval_input_numpy(EVAL_SEED, num_of_elements, INPUT_SIZE['default'])
-        train_and_eval(INPUT_SIZE['default'], GEN_WIDTH['default'], output_size, MAX_VAL['default'],
-                       BATCH_SIZE['default'], GEN_OPT(LEARNING_RATE['default'], BETA1, BETA2),
-                       OPP_OPT(LEARNING_RATE['default'], BETA1, BETA2), steps, eval_data)
-    # vary gen max value
-    for max_val in MAX_VAL['options']:
-        num_of_elements = NUMBER_OF_BITS_PRODUCED / (OUTPUT_SIZE['default'] * round(math.log(max_val, 2)))
-        eval_data = inputs.get_eval_input_numpy(EVAL_SEED, num_of_elements, INPUT_SIZE['default'])
-        train_and_eval(INPUT_SIZE['default'], GEN_WIDTH['default'], OUTPUT_SIZE['default'], max_val,
-                       BATCH_SIZE['default'], GEN_OPT(LEARNING_RATE['default'], BETA1, BETA2),
-                       OPP_OPT(LEARNING_RATE['default'], BETA1, BETA2), steps, eval_data)
-    # vary batch size
-    num_of_elements = NUMBER_OF_BITS_PRODUCED / (OUTPUT_SIZE['default'] * OUTPUT_BITS['default'])
-    eval_data = inputs.get_eval_input_numpy(EVAL_SEED, num_of_elements, INPUT_SIZE['default'])
-    for batch_size in BATCH_SIZE['options']:
-        train_and_eval(INPUT_SIZE['default'], GEN_WIDTH['default'], OUTPUT_SIZE['default'], MAX_VAL['default'],
-                       batch_size, GEN_OPT(LEARNING_RATE['default'], BETA1, BETA2),
-                       OPP_OPT(LEARNING_RATE['default'], BETA1, BETA2), steps, eval_data)
-    # vary learning rate
-    num_of_elements = NUMBER_OF_BITS_PRODUCED / (OUTPUT_SIZE['default'] * OUTPUT_BITS['default'])
-    eval_data = inputs.get_eval_input_numpy(EVAL_SEED, num_of_elements, INPUT_SIZE['default'])
-    for learning_rate in LEARNING_RATE['options']:
-        train_and_eval(INPUT_SIZE['default'], GEN_WIDTH['default'], OUTPUT_SIZE['default'], MAX_VAL['default'],
-                       BATCH_SIZE['default'], GEN_OPT(learning_rate, BETA1, BETA2),
-                       OPP_OPT(learning_rate, BETA1, BETA2), steps, eval_data)
-
-
-def run_discgan(input_size, gen_width, output_size, max_val, batch_size, gen_opt, opp_opt, steps, eval_data):
+def run_discgan(input_size, gen_width, output_size, max_val, batch_size, gen_opt, opp_opt, steps, eval_data, folder):
     """ Constructs, trains and evaluates the discriminative GAN with the provided experiment parameters.
     :param input_size: the dimensionality of the generator input
     :param gen_width: the width of the generator's hidden layers
@@ -156,23 +95,30 @@ def run_discgan(input_size, gen_width, output_size, max_val, batch_size, gen_opt
     :param gen_opt: optimizer used by the generator
     :param opp_opt: optimizer used by the discriminator
     :param steps: number of times the generator/discriminator pair are updated
-    :param eval_data: evaluation dataset used for the generator """
+    :param eval_data: evaluation dataset used for the generator
+    :param folder: subdirectory for experiment run with current parameters """
+
     gan = GAN(input_size, gen_width, output_size, max_val, gen_width, ADV_MULT) \
         .with_distributions(inputs.noise_prior_tf, inputs.reference_distribution_tf) \
         .with_optimizers(gen_opt, opp_opt) \
         .with_loss_functions(losses.generator_classification_loss, losses.discriminator_classification_loss)
-    for i in range(EXPERIMENT_REPETITIONS):
-        print('GAN: training section ' + str(i) + ' -----')
-        gan.train(batch_size, steps)
-        eval_out = gan.predict(eval_data, batch_size)
-        files.write_numbers_to_ascii_file(eval_out, SEQN_DIR + str(i) + '_' + 'gan.txt')
 
+    for i in range(EXPERIMENT_REPETITIONS):
+        for j in range(EVAL_MILESTONES):
+            gan.train(batch_size, steps)
+            eval_out = gan.predict(eval_data, batch_size)
+            files.write_numbers_to_ascii_file(eval_out,
+                                              SEQN_DIR + 'discriminative/' + folder
+                                              + 'steps-' + str(steps)
+                                              + '-exp-' + str(i)
+                                              + '-mile-' + str(j)
+                                              + '-gan.txt')
     # produce output
     files.write_to_file(gan.get_recorded_losses()['generator'], PLOT_DIR + '/generator_loss.txt')
     files.write_to_file(gan.get_recorded_losses()['discriminator'], PLOT_DIR + '/discriminator_loss.txt')
 
 
-def run_predgan(input_size, gen_width, output_size, max_val, batch_size, gen_opt, opp_opt, steps, eval_data):
+def run_predgan(input_size, gen_width, output_size, max_val, batch_size, gen_opt, opp_opt, steps, eval_data, folder):
     """ Constructs, trains and evaluates the predictive GAN with the provided experiment parameters.
     :param input_size: the dimensionality of the generator input
     :param gen_width: the width of the generator's hidden layers
@@ -182,22 +128,88 @@ def run_predgan(input_size, gen_width, output_size, max_val, batch_size, gen_opt
     :param gen_opt: optimizer used by the generator
     :param opp_opt: optimizer used by the predictor
     :param steps: number of times the generator/predictor pair are updated
-    :param eval_data: evaluation dataset used for the generator """
+    :param eval_data: evaluation dataset used for the generator
+    :param folder: subdirectory for experiment run with current parameters """
+
     predgan = PredGAN(input_size, gen_width, output_size, max_val, gen_width, ADV_MULT) \
         .with_distributions(inputs.noise_prior_tf) \
         .with_optimizers(gen_opt, opp_opt) \
         .with_loss_functions(losses.build_generator_regression_loss(max_val),
                              losses.build_predictor_regression_loss(max_val))
-
     for i in range(EXPERIMENT_REPETITIONS):
-        print('GAN - experiment ' + str(i) + ' -----')
-        predgan.train(batch_size, steps)
-        eval_out = predgan.predict(eval_data, batch_size)
-        files.write_numbers_to_ascii_file(eval_out, SEQN_DIR + str(i) + '_' + 'predgan.txt')
-
+        for j in range(EVAL_MILESTONES):
+            predgan.train(batch_size, steps)
+            eval_out = predgan.predict(eval_data, batch_size)
+            files.write_numbers_to_ascii_file(eval_out,
+                                              SEQN_DIR + 'predictive/' + folder
+                                              + 'steps-' + str(steps)
+                                              + '-exp-' + str(i)
+                                              + '-mile-' + str(j)
+                                              + '-predgan.txt')
     # produce output
     files.write_to_file(predgan.get_recorded_losses()['generator'], PLOT_DIR + '/generator_loss.txt')
     files.write_to_file(predgan.get_recorded_losses()['discriminator'], PLOT_DIR + '/discriminator_loss.txt')
+
+
+def run_experiment(train_and_eval):
+    """ Calls the training and evaluation procedure repeatedly, iterating
+    over the possible modified values. """
+    steps = STEPS[0]
+    # defaults
+    num_of_elements = NUMBER_OF_BITS_PRODUCED / (OUTPUT_SIZE['default'] * OUTPUT_BITS['default'])
+    eval_data = inputs.get_eval_input_numpy(EVAL_SEED, num_of_elements, INPUT_SIZE['default'])
+    folder = 'defaults/'
+    train_and_eval(INPUT_SIZE['default'], GEN_WIDTH['default'], OUTPUT_SIZE['default'], MAX_VAL['default'],
+                   BATCH_SIZE['default'], GEN_OPT(LEARNING_RATE['default'], BETA1, BETA2),
+                   OPP_OPT(LEARNING_RATE['default'], BETA1, BETA2), steps, eval_data, folder)
+    # vary input size
+    for input_size in INPUT_SIZE['options']:
+        num_of_elements = NUMBER_OF_BITS_PRODUCED / (OUTPUT_SIZE['default'] * OUTPUT_BITS['default'])
+        eval_data = inputs.get_eval_input_numpy(EVAL_SEED, num_of_elements, input_size)
+        folder = 'input_size/' + str(input_size) + '/'
+        train_and_eval(input_size, GEN_WIDTH['default'], OUTPUT_SIZE['default'], MAX_VAL['default'],
+                       BATCH_SIZE['default'], GEN_OPT(LEARNING_RATE['default'], BETA1, BETA2),
+                       OPP_OPT(LEARNING_RATE['default'], BETA1, BETA2), steps, eval_data, folder)
+    # vary gen width
+    num_of_elements = NUMBER_OF_BITS_PRODUCED / (OUTPUT_SIZE['default'] * OUTPUT_BITS['default'])
+    eval_data = inputs.get_eval_input_numpy(EVAL_SEED, num_of_elements, INPUT_SIZE['default'])
+    for gen_width in GEN_WIDTH['options']:
+        folder = 'num_of_elements/' + str(num_of_elements) + '/'
+        train_and_eval(INPUT_SIZE['default'], gen_width, OUTPUT_SIZE['default'], MAX_VAL['default'],
+                       BATCH_SIZE['default'], GEN_OPT(LEARNING_RATE['default'], BETA1, BETA2),
+                       OPP_OPT(LEARNING_RATE['default'], BETA1, BETA2), steps, eval_data, folder)
+    # vary gen output size
+    for output_size in OUTPUT_SIZE['options']:
+        num_of_elements = NUMBER_OF_BITS_PRODUCED / (output_size * OUTPUT_BITS['default'])
+        eval_data = inputs.get_eval_input_numpy(EVAL_SEED, num_of_elements, INPUT_SIZE['default'])
+        folder = 'output_size/' + str(output_size) + '/'
+        train_and_eval(INPUT_SIZE['default'], GEN_WIDTH['default'], output_size, MAX_VAL['default'],
+                       BATCH_SIZE['default'], GEN_OPT(LEARNING_RATE['default'], BETA1, BETA2),
+                       OPP_OPT(LEARNING_RATE['default'], BETA1, BETA2), steps, eval_data, folder)
+    # vary gen max value
+    for max_val in MAX_VAL['options']:
+        num_of_elements = NUMBER_OF_BITS_PRODUCED / (OUTPUT_SIZE['default'] * round(math.log(max_val, 2)))
+        eval_data = inputs.get_eval_input_numpy(EVAL_SEED, num_of_elements, INPUT_SIZE['default'])
+        folder = 'max_val/' + str(max_val) + '/'
+        train_and_eval(INPUT_SIZE['default'], GEN_WIDTH['default'], OUTPUT_SIZE['default'], max_val,
+                       BATCH_SIZE['default'], GEN_OPT(LEARNING_RATE['default'], BETA1, BETA2),
+                       OPP_OPT(LEARNING_RATE['default'], BETA1, BETA2), steps, eval_data, folder)
+    # vary batch size
+    num_of_elements = NUMBER_OF_BITS_PRODUCED / (OUTPUT_SIZE['default'] * OUTPUT_BITS['default'])
+    eval_data = inputs.get_eval_input_numpy(EVAL_SEED, num_of_elements, INPUT_SIZE['default'])
+    for batch_size in BATCH_SIZE['options']:
+        folder = 'batch_size/' + str(batch_size) + '/'
+        train_and_eval(INPUT_SIZE['default'], GEN_WIDTH['default'], OUTPUT_SIZE['default'], MAX_VAL['default'],
+                       batch_size, GEN_OPT(LEARNING_RATE['default'], BETA1, BETA2),
+                       OPP_OPT(LEARNING_RATE['default'], BETA1, BETA2), steps, eval_data, folder)
+    # vary learning rate
+    num_of_elements = NUMBER_OF_BITS_PRODUCED / (OUTPUT_SIZE['default'] * OUTPUT_BITS['default'])
+    eval_data = inputs.get_eval_input_numpy(EVAL_SEED, num_of_elements, INPUT_SIZE['default'])
+    for learning_rate in LEARNING_RATE['options']:
+        folder = 'learning_rate/' + str(learning_rate) + '/'
+        train_and_eval(INPUT_SIZE['default'], GEN_WIDTH['default'], OUTPUT_SIZE['default'], MAX_VAL['default'],
+                       BATCH_SIZE['default'], GEN_OPT(learning_rate, BETA1, BETA2),
+                       OPP_OPT(learning_rate, BETA1, BETA2), steps, eval_data, folder)
 
 
 if __name__ == '__main__':
